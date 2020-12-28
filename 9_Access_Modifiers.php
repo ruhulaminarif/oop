ffde09e57729ab75e8ed724021b8267bd5e3cdb4
Ruhulamin Arif <33670377+ruhulaminarif@users.noreply.github.com>
2020-12-28 23:09:34 +0600

    Public privet protected

---
+++ 9_Access_Modifiers.php
@@ -0,0 +1,6 @@
+<?php 
+//public = the property or method can be accessed from everywhere. This is default
+//privet = the property or method can be accessed within the class and by classes derived from that class
+//protected = the property or method can ONLY be accessed within the class
+
+ ?>
